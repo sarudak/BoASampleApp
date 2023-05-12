c4b9309e4d9234a90e7b68462634de5c4f6aff06
Mahdi Moqri <mahdi.moqri@gmail.com>
2023-05-12 14:38:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import plotly.express as px
 import streamlit as st
 
+df=pd.read_csv('2010.csv',index_col=0)
+T=df.PERMTH_EXM
+E=df.MORTSTAT
+available_indicators = ['RIDAGEYR','RIAGENDR','LBDGLUSI','Biological Age']
+
 df = px.data.gapminder()
 
 fig = px.scatter(
@@ -14,11 +19,4 @@ fig = px.scatter(
     size_max=60,
 )
 
-tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
-with tab1:
-    # Use the Streamlit theme.
-    # This is the default. So you can also omit the theme argument.
-    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-with tab2:
-    # Use the native Plotly theme.
-    st.plotly_chart(fig, theme=None, use_container_width=True)
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
