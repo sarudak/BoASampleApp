f2b0fb41a17a5080551617ca96b5b7cc94e2c27e
Mahdi Moqri <mahdi.moqri@gmail.com>
2023-05-12 15:09:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,25 @@ if measure=='None':
             #color="RIAGENDR",
             hover_name="Survival",
         )
-
-st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+if measure=='Gender':
+        col=RIAGENDR
+        groups = df[col]
+        ix = (groups == 1)
+        kmf.fit(T[ix], E[ix], label='Male')
+        v1 =kmf.survival_function_
+        kmf.fit(T[~ix], E[~ix], label='Female')
+        v2 =kmf.survival_function_
+        v1.columns=['Survival']
+        v1['c']='Male'
+        v2.columns=['Survival']
+        v2['c']='Female'
+        val=pd.concat([v2,v1])
+        val['Time (months)']=val.index
+        fig = px.line(
+            val,
+            x="Time (months)",
+            y="Survival",
+            color=col,
+            hover_name="Survival",
+        )        
