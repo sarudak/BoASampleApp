7e75aad3c30dd6285bc714768465626c2cfdb8ca
Mahdi Moqri <mahdi.moqri@gmail.com>
2023-05-12 14:48:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,26 @@ import pandas as pd
 from lifelines import KaplanMeierFitter
 kmf = KaplanMeierFitter()
 
+available_indicators = ['RIDAGEYR','RIAGENDR','LBDGLUSI','Biological Age']
+
 df=pd.read_csv('2010.csv',index_col=0)
 T=df.PERMTH_EXM
 E=df.MORTSTAT
-available_indicators = ['RIDAGEYR','RIAGENDR','LBDGLUSI','Biological Age']
-
+kmf.fit(T, E)  
+val=kmf.survival_function_
+val.columns=['Survival']
+val['Time (months)']=val.index  
+        
 fig = px.line(
-    df,
-    x="RIDAGEYR",
-    y="Biological Age",
-    color="RIAGENDR",
-    hover_name="Biological Age",
+    val,
+    x="Time (months)",
+    y="Survival",
+    #color="RIAGENDR",
+    hover_name="Survival",
 )
 
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+
+      
+        fig = px.line(val, 
+            x='Time (months)', y="Survival")
