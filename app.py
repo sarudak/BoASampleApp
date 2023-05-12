4b455ca6fa7b20bd2b2f52420e7acb10150e9d9e
Mahdi Moqri <mahdi.moqri@gmail.com>
2023-05-12 14:02:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+import plotly.express as px
 import streamlit as st
 
-st.header('st.button')
+df = px.data.gapminder()
 
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+fig = px.scatter(
+    df.query("year==2007"),
+    x="gdpPercap",
+    y="lifeExp",
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=60,
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
