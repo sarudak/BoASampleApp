6288deb11c5ed086dbd183709d44df473d881739
Mahdi Moqri <mahdi.moqri@gmail.com>
2023-05-12 15:32:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,60 +13,47 @@ measure = st.selectbox('Category', measures)
 df=pd.read_csv('2010.csv',index_col=0)
 T=df.PERMTH_EXM
 E=df.MORTSTAT
-if measure=='None':
-        kmf.fit(T, E)  
-        val=kmf.survival_function_
-        val.columns=['Survival']
-        val['Time (months)']=val.index  
-        fig = px.line(
-            val,
-            x="Time (months)",
-            y="Survival",
-            #color="RIAGENDR",
-            hover_name="Survival",
-        )
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-if measure=='Gender':
-        col='RIAGENDR'
-        groups = df[col]
-        ix = (groups == 1)
-        kmf.fit(T[ix], E[ix], label='Male')
-        v1 =kmf.survival_function_
-        kmf.fit(T[~ix], E[~ix], label='Female')
-        v2 =kmf.survival_function_
-        v1.columns=['Survival']
-        v1['Category']='Male'
-        v2.columns=['Survival']
-        v2['Category']='Female'
-        val=pd.concat([v2,v1])
-        val['Time (months)']=val.index
-        fig = px.line(
-            val,
-            x="Time (months)",
-            y="Survival",
-            color='Category',
-            hover_name="Survival",
-        )  
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
-if measure=='Blood glucose':
-        col='LBDGLUSI'        
-        groups = df[col]
-        ix = (groups <5.5)
-        kmf.fit(T[ix], E[ix], label='Low Glucose')
-        v1 =kmf.survival_function_
-        kmf.fit(T[~ix], E[~ix], label='High Glucose')
-        v2 =kmf.survival_function_
-        v1.columns=['Survival']
-        v1['Category']='Low Glucose'
-        v2.columns=['Survival']
-        v2['Category']='High Glucose'
-        val=pd.concat([v1,v2])
-        val['Time (months)']=val.index
-        fig = px.line(
-            val,
-            x="Time (months)",
-            y="Survival",
-            color='Category',
-            hover_name="Survival",
-        )  
-        st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+def update_fig(measure)
+        if measure=='None':
+                kmf.fit(T, E)  
+                val=kmf.survival_function_
+                val.columns=['Survival']
+                val['Time (months)']=val.index
+        if measure=='Gender':
+                col='RIAGENDR'
+                groups = df[col]
+                ix = (groups == 1)
+                kmf.fit(T[ix], E[ix], label='Male')
+                v1 =kmf.survival_function_
+                kmf.fit(T[~ix], E[~ix], label='Female')
+                v2 =kmf.survival_function_
+                v1.columns=['Survival']
+                v1['Category']='Male'
+                v2.columns=['Survival']
+                v2['Category']='Female'
+                val=pd.concat([v2,v1])
+                val['Time (months)']=val.index  
+        if measure=='Blood glucose':
+                col='LBDGLUSI'        
+                groups = df[col]
+                ix = (groups <5.5)
+                kmf.fit(T[ix], E[ix], label='Low Glucose')
+                v1 =kmf.survival_function_
+                kmf.fit(T[~ix], E[~ix], label='High Glucose')
+                v2 =kmf.survival_function_
+                v1.columns=['Survival']
+                v1['Category']='Low Glucose'
+                v2.columns=['Survival']
+                v2['Category']='High Glucose'
+                val=pd.concat([v1,v2])
+                val['Time (months)']=val.index                
+        return val
+val= update_fig(measure)
+fig = px.line(
+    val,
+    x="Time (months)",
+    y="Survival",
+    #color="RIAGENDR",
+    hover_name="Survival",
+)
+st.plotly_chart(fig, theme="streamlit", use_container_width=True)
